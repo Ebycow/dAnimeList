4abeade79156b556149ffc4a2e9c8f74678cfd95
えびかう(Eby) <18446038+Ebycow@users.noreply.github.com>
2019-05-12 14:12:44 +0900

    Create dAnimeList.user.js

---
+++ dAnimeList.user.js
@@ -0,0 +1,36 @@
+// ==UserScript==
+// @name         dAnimeList
+// @namespace    https://ebycow.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       Ebycow <https://ebycow.net>
+// @match        https://anime.dmkt-sp.jp/animestore/sc_d_pc?partId=*
+// @grant        GM_xmlhttpRequest
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    $(() => {
+        $('.popupIn').append($('<div class="list mal"><div class="w2 caption">MyAnimeList</div></div>'))
+
+        $('.mal').click(() => {
+            const title = $('.pauseInfoTxt1').text();
+            const limit = 1;
+
+            GM_xmlhttpRequest({
+                method : 'GET',
+                url : `https://api.jikan.moe/v3/search/anime?q=${ title }&limit=${ limit }`,
+                onload : (response) => {
+                    const suggestAnimes = JSON.parse(response.responseText).results;
+                    window.open(suggestAnimes[0].url,"_blank");
+
+                }
+            });
+
+        });
+
+    });
+
+})();
